1142bb686ccc7c7a6957f9657af0e5496e51f628
srdothunter <wendy8449@gmail.com>
2021-03-20 21:16:14 +0800

    fix contract address

--- public/js/contractAddress.js
+++ public/js/contractAddress.js
@@ -1,6 +1,6 @@
 const mainAddress = {
-    market: "5F3Si82Y4Lsyp4sC2tAk7ChoJorPcokkwwFTNQ7j4YYorB8q" ,
-    statistics:'5Fteh8ZEvTSydVY2ziQDmyY6ZuSzAdqiRwCNR5rRyWBMRTTA',
+    market: "5CSwdmqvqkHm8JeBvgECwMwg1oosG2H92LsoFuSMAxitqP9Z" ,
+    statistics:'5HF3jRkazUBSn3zkA5mNZPZPSGnYC8c3meL1uzo7U6t44KA5',
     basepath:'192.168.1.89',
     name:'Apron'
 };
