fc906ad404c38fb051586a10ebd6c2de0f2bbf99
Apron2050 <74755044+Apron2050@users.noreply.github.com>
2021-08-12 02:42:00 +0800

    update to use new contracts

--- public/js/contractAddress.js
+++ public/js/contractAddress.js
@@ -1,6 +1,6 @@
 const configuration = {
-    market: "5H9vNngQCA48JE1ekGaYbGs44KyPhvQuWjf5EzRibCFjm9Wz",
-    statistics:'5HEe8yzYH6DNw8cTHWUK8qvfYotj5BihwMyAFZ7pRKo2qjks',
+    market: "5DupAxgDoNzYt3j1dpJnpsAML3TCZwdeh2xem8Kx52nBaGA4",
+    statistics:'5G727fGbE7uAx4BAB8SF6yzA6KhNwn6RDppMvqs4eMEFhhQY',
     basepath:'47.242.250.114',
     name:'Apron Market'
 };
