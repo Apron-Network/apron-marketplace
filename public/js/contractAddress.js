6bff884d8d35bf7848069d9106ead3435ddb6179
Apron2050 <74755044+Apron2050@users.noreply.github.com>
2021-08-11 19:07:32 +0800

    update to use new address

--- public/js/contractAddress.js
+++ public/js/contractAddress.js
@@ -1,7 +1,7 @@
 const mainAddress = {
-    market: "5CTBtLMvYacY29uPvNypRqL96uMAAifnrGysSDsLGThgcCa2",
-    statistics:'5DwcMq4S9MB64NBUKFmCKH4Py1dyQ3ftoHjxGPr8oH7WAMUr',
-    basepath:'m1-alice.apron.network',
+    market: "5H9vNngQCA48JE1ekGaYbGs44KyPhvQuWjf5EzRibCFjm9Wz",
+    statistics:'5HEe8yzYH6DNw8cTHWUK8qvfYotj5BihwMyAFZ7pRKo2qjks',
+    basepath:'47.242.250.114',
     name:'Apron Market'
 };
 window.mainAddress = mainAddress;
